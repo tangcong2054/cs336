9fc612db82d7ac129468dadba73ac28d5fef7cea
97 <348876414@qq.com>
2025-07-27 16:20:20 +0800

    删除多个文件，包括配置文件、目录文件、笔记本和测试文件，清理项目结构以提升可维护性；更新.gitignore以排除不必要的文件；移除与项目无关的资源，确保代码库的整洁。

--- code/assignment1-basics/cs336_basics/p1_tokenizer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .bpe_batcher import BPETokenizerBatcher
-from .bpe_tokenizer import BPETokenizer
-from .bpe_trainer import BPETrainer
-
-__all__ = [
-    "BPETokenizerBatcher",
-    "BPETokenizer",
-    "BPETrainer"
-]
